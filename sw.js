02891ee9c080d75dfe7eb3f623265ad12f04fbdb
ALDINEI12 <nbdynei707@gmail.com>
2026-08-02 12:05:51 -0300

    kosp

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ALDINEICAR Service Worker — cache estático + rede para API */
-const CACHE_NAME = 'aldineicar-v7';
+const CACHE_NAME = 'aldineicar-v9';
 const ASSETS = [
   './',
   './index.html',
