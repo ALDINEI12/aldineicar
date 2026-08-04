934ec59c93ce58e94617822e798c9e537182b726
ALDINEI12 <nbdynei707@gmail.com>
2026-08-03 21:59:35 -0300

    dsdkl

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ALDINEICAR Service Worker — cache estático + rede para API */
-const CACHE_NAME = 'aldineicar-v20';
+const CACHE_NAME = 'aldineicar-v22';
 const ASSETS = [
   './',
   './index.html',
