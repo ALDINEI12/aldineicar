ce7bf9a7268b690fec0f47efe3976093440ca101
ALDINEI12 <nbdynei707@gmail.com>
2026-08-10 20:43:32 -0300

    ldspi

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ALDINEICAR Service Worker — cache estático + rede para API */
-const CACHE_NAME = 'aldineicar-v45';
+const CACHE_NAME = 'aldineicar-v46';
 const ASSETS = [
   './',
   './index.html',
