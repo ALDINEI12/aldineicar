aa02fba19a21a535ca6c96292221768ee18897ae
ALDINEI12 <nbdynei707@gmail.com>
2026-08-10 19:47:22 -0300

    ksop

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ALDINEICAR Service Worker — cache estático + rede para API */
-const CACHE_NAME = 'aldineicar-v37';
+const CACHE_NAME = 'aldineicar-v40';
 const ASSETS = [
   './',
   './index.html',
